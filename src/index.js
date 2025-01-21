9e7a65cfeb8acfe39f929d86287fa69c54c99d86
Nani-Prabhakar <nanibramhasamudram@gmail.com>
2025-01-21 19:30:21 +0530

    created database for flights using sequlize -cli

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const { config } = require('dotenv');
+const bodyParser=require('body-parser');
 const express=require('express');
 const {PORT}= require('./config/serverConfig')
 const setupAndStart=async ()=>{
     const app=express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
     app.listen(PORT,()=>{
         console.log(`server is running at port ${PORT} `)
     })
 }
 
-setupAndStart();
+setupAndStart()
