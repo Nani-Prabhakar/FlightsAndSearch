1d46a214ddbdedad40f05661a4ef5b41be84451d
Nani-Prabhakar <nanibramhasamudram@gmail.com>
2025-01-22 11:03:00 +0530

    first database action

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { config } = require('dotenv');
+const cityRepository=require("./repository/city_repository");
 const bodyParser=require('body-parser');
 const express=require('express');
 const {PORT}= require('./config/serverConfig')
@@ -6,9 +7,13 @@ const setupAndStart=async ()=>{
     const app=express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
-    app.listen(PORT,()=>{
+    app.listen(PORT,async()=>{
         console.log(`server is running at port ${PORT} `)
+        const repo=new cityRepository();
+        await repo.createCity({name:"new delhi"});
+       
     })
+    
 }
 
 setupAndStart()
