cbf652b0a084b61359368e56e0c9ce84b0fa4248
Nani-Prabhakar <nanibramhasamudram@gmail.com>
2025-01-31 00:39:23 +0530

    implemented and tested first flight service city

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 const { config } = require('dotenv');
-const cityRepository=require("./repository/city_repository");
+const cityRepository=require("./repository/city-repository");
 const bodyParser=require('body-parser');
 const express=require('express');
 const {PORT}= require('./config/serverConfig')
+
+const ApiRoutes = require('./routes/index');
 const setupAndStart=async ()=>{
+
     const app=express();
+
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
+    app.use('/api',ApiRoutes);
+
     app.listen(PORT,async()=>{
         console.log(`server is running at port ${PORT} `)
-        const repo=new cityRepository();
-        await repo.createCity({name:"new delhi"});
+        // const repo=new cityRepository();
+        // await repo.createCity({name:"new delhi"});
        
     })
     
---
+++ src/repository/index.js
@@ -0,0 +1,9 @@
+// const AirplaneRepository = require('./airplane-repository');
+
+module.exports={
+    CityRepository:require('./city-repository'),
+    // AirportRepository:require('./airport-repository'),
+    // FlightRepository:require('./flight-repository'),
+    // AirplaneRepository:require('./airplane-repository'),
+    // CrudRepository:require('./crud-repository')
+}
